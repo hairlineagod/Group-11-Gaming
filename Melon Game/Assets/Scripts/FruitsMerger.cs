8e734ce5ec330f7b1cd33719fc745e3af055e3ba
hairlineagod <timmiexyooj@gmail.com>
2023-12-04 20:09:33 -0800

    Fruits can finally merge
    fruits merge!

--- Melon Game/Assets/Scripts/FruitsMerger.cs
+++ Melon Game/Assets/Scripts/FruitsMerger.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class FruitsMerger : MonoBehaviour
 {
-    public GameObject Strawberry;
+
     public GameObject player;
-    public SpawnFruit spawnFruit;
     // Start is called before the first frame update
     void Awake()
     {
@@ -26,7 +25,86 @@ public class FruitsMerger : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             gameObject.SetActive(false);
-            spawnFruit.spawned = true;
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 1;
+        }
+        if (other.tag == "Strawberry" && gameObject.tag == "Strawberry")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 2;
+        }
+        if (other.tag == "Grape" && gameObject.tag == "Grape")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 3;
+        }
+        if (other.tag == "Dekopon" && gameObject.tag == "Dekopon")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 4;
+        }
+        if (other.tag == "Orange" && gameObject.tag == "Orange")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 5;
+        }
+        if (other.tag == "Apple" && gameObject.tag == "Apple")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 6;
+        }
+        if (other.tag == "Pear" && gameObject.tag == "Pear")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 7;
+        }
+        if (other.tag == "Peach" && gameObject.tag == "Peach")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 8;
+        }
+        if (other.tag == "Pineapple" && gameObject.tag == "Pineapple")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 9;
+        }
+        if (other.tag == "Melon" && gameObject.tag == "Melon")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+            player.GetComponent<PlayerController>().spawned = true;
+            player.GetComponent<PlayerController>().spawnPos = transform.position;
+            player.GetComponent<PlayerController>().whichFruit = 10;
+        }
+        if (other.tag == "Watermelon" && gameObject.tag == "Watermelon")
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
--- Melon Game/Assets/Scripts/PlayerController.cs
+++ Melon Game/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
-    public GameObject Cherry;
-    public GameObject Strawberry;
+    public GameObject[] fruits;
+    public int whichFruit;
+    public bool spawned = false;
+    public Vector3 spawnPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +32,13 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Pressed Space");
-            Instantiate(Cherry, transform.position, new Quaternion(-90, 0, 0, -90));
+            Instantiate(fruits[0], transform.position, new Quaternion(-90, 0, 0, -90));
+        }
+        if (spawned == true)
+        {
+            Debug.Log("spawning");
+            Instantiate(fruits[whichFruit], spawnPos, new Quaternion(-90, 0, 0, -90));
+            spawned = false;
         }
 
     }
